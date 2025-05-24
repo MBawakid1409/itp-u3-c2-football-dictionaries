b293d46219c3e831a7becd1cd4ec4af1651fe4a0
Mohammad Omar <62749199+MBawakid1409@users.noreply.github.com>
2025-05-24 11:05:29 +0300

    Update assignment_2.py
    import required modules

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,3 +1,6 @@
+from squads_data import SQUADS_DATA
+from football_dictionaries.assignment_1.py import players_as_dictionaries
+
 def players_by_position(squads_list):
     players = players_as_dictionaries(squads_list)
     positions_dict = {}
