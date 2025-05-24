915f9534d93918caa02666d792ce3f293f9d15b3
Mohammad Omar <62749199+MBawakid1409@users.noreply.github.com>
2025-05-24 10:08:02 +0300

    Update assignment_2.py
    Add players to their positions list

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,11 @@
 def players_by_position(squads_list):
-    pass
+    players = players_as_dictionaries(squads_list)
+    positions_dict = {}
+    for player in players:
+        position = player['position']
+        if position not in positions_dict:
+            positions_dict[position] = []
+        positions_dict[position].append(player)
+    return positions_dict
+
+players_by_position(SQUADS_DATA)
