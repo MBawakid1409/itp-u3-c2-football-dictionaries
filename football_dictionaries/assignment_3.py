4d539177a5c8c952bc03e07d79de6a962557ede8
Mohammad Omar <62749199+MBawakid1409@users.noreply.github.com>
2025-05-24 11:06:23 +0300

    Update assignment_3.py
    import required modules

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,3 +1,6 @@
+from squads_data import SQUADS_DATA
+from football_dictionaries.assignment_2.py import players_as_dictionaries
+
 def players_by_country_and_position(squads_list):
     players = players_as_dictionaries(squads_list)
     country_dict = {}   
