2b553a81e09f0272061d901c38de4096991f8245
Mohammad Omar <62749199+MBawakid1409@users.noreply.github.com>
2025-05-24 09:53:41 +0300

    Update assignment_1.py
    add the code / solve 1st assignment

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,37 @@
 def players_as_dictionaries(squads_list):
-    pass
+    result = []
+    keys = [
+        'number',
+        'position',
+        'name',
+        'date_of_birth',
+        'caps',
+        'club',
+        'country',
+        'club_country',
+        'year'
+    ]
+    
+    for player in squads_list:
+        player_dict = {}
+        for i, value in enumerate(player):
+            player_dict[keys[i]] = value
+        result.append(player_dict)
+    
+    return result
+
+# or with zip function
+
+# def players_as_dictionaries(squads_list):
+#     keys = [
+#         'number',
+#         'position',
+#         'name',
+#         'date_of_birth',
+#         'caps',
+#         'club',
+#         'country',
+#         'club_country',
+#         'year'
+#     ]
+#     return [dict(zip(keys, player)) for player in squads_list]
